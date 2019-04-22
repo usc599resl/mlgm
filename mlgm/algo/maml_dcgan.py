b764a70daad45ffe9c13374daf8cd2a7e031a60d
Angel Gonzalez <aigonzal@usc.edu>
2019-04-22 02:08:53 -0700

    Remove second order derivative from MAML-DCGAN

--- mlgm/algo/maml_dcgan.py
+++ mlgm/algo/maml_dcgan.py
@@ -43,99 +43,79 @@ class MamlDcGan:
             self._itr_in = tf.placeholder(dtype=tf.float32, shape=())
             input_a, label_a, input_b, label_b = \
                 self._metasampler.build_train_inputs_and_labels()
-            train_loss_a, train_losses_b, output = self._build(
-                input_a,
-                label_a,
-                input_b,
-                label_b,
-                self._metasampler.train_meta_batch_size,
+            train_losses_a, output = self._build(
+                input_a, label_a, self._metasampler.train_meta_batch_size,
                 training=True)
 
-            self._output = output
+            self._train_output = output
 
+            self._d_train_losses_a = [i[0] for i in train_losses_a]
+            self._g_train_losses_a = [i[1] for i in train_losses_a]
             dis_vars = self._model.get_variables()["discriminator"]
             gen_vars = self._model.get_variables()["generator"]
-            with tf.variable_scope("pretrain", values=[train_loss_a]):
-                self._d_train_loss_a = train_loss_a[0]
-                self._g_train_loss_a = train_loss_a[1]
+            with tf.variable_scope("pretrain", values=[train_losses_a]):
                 self._dis_pretrain_op = tf.train.AdamOptimizer(
                     self._meta_lr, 0.5).minimize(
-                        self._d_train_loss_a, var_list=dis_vars)
+                        self._d_train_losses_a[0], var_list=dis_vars)
                 self._gen_pretrain_op = tf.train.AdamOptimizer(
                     self._meta_lr, 0.5).minimize(
-                        self._g_train_loss_a, var_list=gen_vars)
+                        self._g_train_losses_a[0], var_list=gen_vars)
 
-            with tf.variable_scope("metatrain", values=[train_losses_b]):
-                self._d_train_losses_b = [i[0] for i in train_losses_b]
-                self._g_train_losses_b = [i[1] for i in train_losses_b]
+            with tf.variable_scope("metatrain", values=[train_losses_a]):
                 self._dis_metatrain_op = tf.train.AdamOptimizer(
                     self._meta_lr, 0.5).minimize(
-                        self._d_train_losses_b[-1], var_list=dis_vars)
+                        self._d_train_losses_a[-1], var_list=dis_vars)
                 self._gen_metatrain_op = tf.train.AdamOptimizer(
                     self._meta_lr, 0.5).minimize(
-                        self._g_train_losses_b[-1], var_list=gen_vars)
+                        self._g_train_losses_a[-1], var_list=gen_vars)
 
     def _build_test(self):
         with self._sess.graph.as_default():
             input_a, label_a, input_b, label_b = \
                     self._metasampler.build_test_inputs_and_labels()
-            test_loss_a, test_losses_b, output = self._build(
-                input_a, label_a, input_b, label_b,
-                self._metasampler.test_meta_batch_size)
-            self._d_test_loss_a = test_loss_a[0]
-            self._g_test_loss_a = test_loss_a[1]
-            self._d_test_losses_b = [i[0] for i in test_losses_b]
-            self._g_test_losses_b = [i[1] for i in test_losses_b]
+            test_losses_a, output = self._build(
+                input_a, label_a, self._metasampler.test_meta_batch_size)
+            self._d_test_losses_a = [i[0] for i in test_losses_a]
+            self._g_test_losses_a = [i[1] for i in test_losses_a]
+            self._test_output = output
 
     def _build(self,
                dataset_in_a,
                dataset_lb_a,
-               dataset_in_b,
-               dataset_lb_b,
                num_parallel_itr,
                training=False):
         def task_metalearn(args):
-            input_a, label_a, input_b, label_b = args
+            input_a, label_a = args
             self._model.build_forward_pass(input_a, training=training)
 
-            loss_a = None
-            losses_b = []
+            losses_a = []
             f_w = None
-            output_b = None
+            outputs = None
             for i in range(self._num_updates):
-                loss, loss_b, f_w, output_b = self._build_update(
-                    input_a, label_a, input_b, label_b, self._update_lr, f_w,
-                    training)
-                if loss_a is None:
-                    loss_a = (tf.math.reduce_mean(loss[0]),
-                              tf.math.reduce_mean(loss[1]))
-                losses_b.append((tf.math.reduce_mean(loss_b[0]),
-                                 tf.math.reduce_mean(loss_b[1])))
+                loss_a, f_w, outputs = self._build_update(
+                    input_a, label_a, self._update_lr, f_w, training)
+                losses_a.append((tf.math.reduce_mean(loss_a[0]),
+                                 tf.math.reduce_mean(loss_a[1])))
 
-            return loss_a, losses_b, output_b[0]
+            return losses_a, outputs
 
-        out_dtype = ((tf.float32, ) * 2,
-                     [(tf.float32, ) * 2] * self._num_updates, tf.float32)
-        elems = (dataset_in_a, dataset_lb_a, dataset_in_b, dataset_lb_b)
-        loss_a, losses_b, output_b = tf.map_fn(
+        out_dtype = ([(tf.float32, ) * 2] * self._num_updates, tf.float32)
+        elems = (dataset_in_a, dataset_lb_a)
+        losses_a, outputs_a = tf.map_fn(
             task_metalearn,
             elems=elems,
             dtype=out_dtype,
             parallel_iterations=num_parallel_itr)
 
-        return loss_a, losses_b, output_b
+        return losses_a, outputs_a
 
     def _build_update(self,
                       input_a,
                       label_a,
-                      input_b,
-                      label_b,
                       update_lr,
                       fast_weights=None,
                       training=False):
-        values = [input_a, label_a, input_b, label_b, update_lr]
-        loss_a = None
-        loss_b = None
+        values = [input_a, label_a, update_lr]
         with tf.variable_scope("update", values=values):
             output_a = self._model.build_forward_pass(input_a, fast_weights,
                                                       training)
@@ -146,45 +126,32 @@ class MamlDcGan:
                     w: weights[w] - update_lr * grads[w]
                     for w in weights
                 }
-            output_b = self._model.build_forward_pass(
-                input_b, new_fast_weights, training)
-            loss_b = self._model.build_loss(label_b, output_b)
-        return loss_a, loss_b, new_fast_weights, output_b
+        return loss_a, new_fast_weights, output_a[0]
 
     def _compute_dis_pretrain(self):
-        loss_a, _ = self._sess.run([self._d_train_loss_a,
+        loss_a, _ = self._sess.run([self._d_train_losses_a[0],
             self._dis_pretrain_op])
         return loss_a
 
     def _compute_gen_pretrain(self):
-        loss_a, _ = self._sess.run([self._g_train_loss_a,
+        loss_a, _ = self._sess.run([self._g_train_losses_a[0],
             self._gen_pretrain_op])
         return loss_a
 
     def _compute_dis_metatrain(self, itr):
-        loss_a, losses_b, _ = self._sess.run([
-            self._d_train_loss_a, self._d_train_losses_b,
-            self._dis_metatrain_op
-        ], {self._itr_in: itr})
-        return loss_a, losses_b
+        losses_a, _ = self._sess.run([self._d_train_losses_a,
+            self._dis_metatrain_op], {self._itr_in: itr})
+        return losses_a
 
     def _compute_gen_metatrain(self, itr):
-        loss_a, losses_b, output, _ = self._sess.run([
-            self._g_train_loss_a, self._g_train_losses_b, self._output,
-            self._gen_metatrain_op
-        ], {self._itr_in: itr})
-        return loss_a, losses_b, output
-
-    def _test_prelearner(self):
-        loss_a = self._sess.run([self._test_loss_a])
-        return loss_a
+        losses_a, output, _ = self._sess.run([self._g_train_losses_a,
+            self._train_output, self._gen_metatrain_op], {self._itr_in: itr})
+        return losses_a, output
 
     def _test_metalearner(self):
-        g_loss_a, g_losses_b, d_loss_a, d_losses_b = self._sess.run([
-            self._g_test_loss_a, self._g_test_losses_b, self._d_test_loss_a,
-            self._d_test_losses_b
-        ])
-        return g_loss_a, g_losses_b, d_loss_a, d_losses_b
+        g_losses_a, d_losses_a, output = self._sess.run([self._g_test_losses_a,
+            self._d_test_losses_a, self._test_output])
+        return g_losses_a, d_losses_a, output
 
     def train(self, restore_model_path=None):
         self._sess.run(tf.global_variables_initializer())
@@ -194,14 +161,10 @@ class MamlDcGan:
         self._metasampler.restart_train_dataset(self._sess)
         self._metasampler.restart_test_dataset(self._sess)
         for i in range(self._pretrain_itr + self._metatrain_itr):
-            test_dis_loss_a = []
-            test_dis_losses_b = []
-            test_gen_loss_a = []
-            test_gen_losses_b = []
-            train_dis_loss_a = []
-            train_dis_losses_b = []
-            train_gen_loss_a = []
-            train_gen_losses_b = []
+            test_dis_losses_a = []
+            test_gen_losses_a = []
+            train_dis_losses_a = []
+            train_gen_losses_a = []
             if i < self._pretrain_itr:
                 self._metasampler.restart_train_dataset(self._sess)
                 dis_loss_a = self._compute_dis_pretrain()
@@ -213,48 +176,44 @@ class MamlDcGan:
                 self._logger.dump_summary(i)
             else:
                 self._metasampler.restart_train_dataset(self._sess)
-                dis_loss_a, dis_losses_b = \
-                    self._compute_dis_metatrain(i)
+                dis_losses_a = self._compute_dis_metatrain(i)
                 self._metasampler.restart_train_dataset(self._sess)
-                gen_loss_a, gen_losses_b, output = \
-                    self._compute_gen_metatrain(i)
-                train_dis_loss_a.append(np.mean(dis_loss_a))
-                train_dis_losses_b.append(np.array(dis_losses_b).mean(axis=1))
-                train_gen_loss_a.append(np.mean(gen_loss_a))
-                train_gen_losses_b.append(np.array(gen_losses_b).mean(axis=1))
+                gen_losses_a, train_outputs = self._compute_gen_metatrain(i)
+                train_dis_losses_a.append(np.array(dis_losses_a).mean(axis=1))
+                train_gen_losses_a.append(np.array(gen_losses_a).mean(axis=1))
 
                 self._metasampler.restart_test_dataset(self._sess)
-                g_loss_a, g_losses_b, d_loss_a, d_losses_b = \
-                    self._test_metalearner()
-                test_dis_loss_a.append(np.mean(d_loss_a))
-                test_dis_losses_b.append(np.array(d_losses_b).mean(axis=1))
-                test_gen_loss_a.append(np.mean(g_loss_a))
-                test_gen_losses_b.append(np.array(g_losses_b).mean(axis=1))
+                g_losses_a, d_losses_a, test_output = self._test_metalearner()
+                test_dis_losses_a.append(np.array(d_losses_a).mean(axis=1))
+                test_gen_losses_a.append(np.array(g_losses_a).mean(axis=1))
 
-                train_dis_loss_a = np.array(train_dis_loss_a).mean()
-                train_dis_losses_b = np.array(train_dis_losses_b).mean(axis=0)
-                train_gen_loss_a = np.array(train_gen_loss_a).mean()
-                train_gen_losses_b = np.array(train_gen_losses_b).mean(axis=0)
-                test_dis_loss_a = np.array(test_dis_loss_a).mean()
-                test_dis_losses_b = np.array(test_dis_losses_b).mean(axis=0)
-                test_gen_loss_a = np.array(test_gen_loss_a).mean()
-                test_gen_losses_b = np.array(test_gen_losses_b).mean(axis=0)
+                train_dis_losses_a = np.array(train_dis_losses_a).mean(axis=0)
+                train_gen_losses_a = np.array(train_gen_losses_a).mean(axis=0)
+                test_dis_losses_a = np.array(test_dis_losses_a).mean(axis=0)
+                test_gen_losses_a = np.array(test_gen_losses_a).mean(axis=0)
                 self._logger.new_summary()
-                self._logger.add_value("train_dis_loss_a", train_dis_loss_a)
-                self._logger.add_value("train_dis_losses_b/update_",
-                                       train_dis_losses_b.tolist())
-                self._logger.add_value("train_gen_loss_a", train_gen_loss_a)
-                self._logger.add_value("train_gen_losses_b/update_",
-                                       train_gen_losses_b.tolist())
-                self._logger.add_value("test_dis_loss_a", test_dis_loss_a)
-                self._logger.add_value("test_dis_losses_b/update_",
-                                       test_dis_losses_b.tolist())
-                self._logger.add_value("test_gen_loss_a", test_gen_loss_a)
-                self._logger.add_value("test_gen_losses_b/update_",
-                                       test_gen_losses_b.tolist())
-                imgs = transform_img(output[0])
-                img_fig = get_img_from_arr(imgs)
-                self._logger.add_img("output_mnist", img_fig)
+                self._logger.add_value("train_dis_losses_a/update_",
+                                       train_dis_losses_a.tolist())
+                self._logger.add_value("train_gen_losses_a/update_",
+                                       train_gen_losses_a.tolist())
+                self._logger.add_value("test_dis_losses_a/update_",
+                                       test_dis_losses_a.tolist())
+                self._logger.add_value("test_gen_losses_a/update_",
+                                       test_gen_losses_a.tolist())
+
+                if i % 100:
+                    for j, out in enumerate(train_outputs):
+                        imgs = transform_img(out)
+                        img_fig = get_img_from_arr(imgs)
+                        self._logger.add_img(
+                                "train_output_".format(j), img_fig)
+
+                    for j, out in enumerate(test_output):
+                        imgs = transform_img(out)
+                        img_fig = get_img_from_arr(imgs)
+                        self._logger.add_img(
+                                "test_output_".format(j), img_fig)
+
                 self._logger.dump_summary(i)
         self._logger.save_tf_variables(self._model.get_variables(), i,
                                        self._sess)
